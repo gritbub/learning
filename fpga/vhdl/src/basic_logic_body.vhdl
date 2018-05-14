83562da453b35f9eb7074e2c08ab0329256a72f1
gritbub <38131016+gritbub@users.noreply.github.com>
2018-05-14 06:00:43 -0500

    Continue to build basic_logic package

--- fpga/vhdl/src/basic_logic_body.vhdl
+++ fpga/vhdl/src/basic_logic_body.vhdl
@@ -14,9 +14,7 @@
 --				:
 --	Limitation	:	See original below.
 --				:
---	Note		:	Copied from parts of ieee.std_logic_1164. See
---				:	original documenation for std_logic_1164 at the end
---				:	of this file.
+--	Note		:	Copied from parts of ieee.std_logic_1164.
 --
 ------------------------------------------------------------------------
 
@@ -124,39 +122,3 @@ package body basic_logic is
 	
 end basic_logic;
 
-
--- Copy of original documenation:
--- --------------------------------------------------------------------
---
---   Title     :  std_logic_1164 multi-value logic system
---   Library   :  This package shall be compiled into a library
---             :  symbolically named IEEE.
---             :
---   Developers:  IEEE model standards group (par 1164)
---   Purpose   :  This packages defines a standard for designers
---             :  to use in describing the interconnection data types
---             :  used in vhdl modeling.
---             :
---   Limitation:  The logic system defined in this package may
---             :  be insufficient for modeling switched transistors,
---             :  since such a requirement is out of the scope of this
---             :  effort. Furthermore, mathematics, primitives,
---             :  timing standards, etc. are considered orthogonal
---             :  issues as it relates to this package and are therefore
---             :  beyond the scope of this effort.
---             :
---   Note      :  No declarations or definitions shall be included in,
---             :  or excluded from this package. The "package declaration"
---             :  defines the types, subtypes and declarations of
---             :  std_logic_1164. The std_logic_1164 package body shall be
---             :  considered the formal definition of the semantics of
---             :  this package. Tool developers may choose to implement
---             :  the package body in the most efficient manner available
---             :  to them.
---             :
--- --------------------------------------------------------------------
---   modification history :
--- --------------------------------------------------------------------
---  version | mod. date:|
---   v4.200 | 01/02/91  |
--- --------------------------------------------------------------------
